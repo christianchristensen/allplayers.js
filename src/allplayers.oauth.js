5112d25dff5907415093422b78c7589f72e64479
Chris Christensen <christian.christensen@gmail.com>
2012-04-17 01:50:34 -0500

    Step by step allplayers specific oauth js impl.

--- src/allplayers.oauth.js
+++ src/allplayers.oauth.js
@@ -5,8 +5,8 @@ var allplayers = allplayers || {};
 allplayers.consumer = allplayers.consumer || {};
 
 allplayers.consumer.credentials = {
-  consumerKey    : "AWeKC3PVG7DmP8JcgFgACqAL4tz3HkGD",
-  consumerSecret : "XCGYQS9noNt3wJxDKJ3CTmSa8mA8v3gs",
+  consumerKey    : "",
+  consumerSecret : "",
   tokenKey    : "",
   tokenSecret : "",
   serviceProvider: {
@@ -17,35 +17,95 @@ allplayers.consumer.credentials = {
   }
 };
 
+// TODO: collapse these into one function
 allplayers.consumer.request_token =
-function request_token(data) {
-    var accessor = {
-      consumerSecret: allplayers.consumer.credentials.consumerSecret,
-      tokenSecret   : allplayers.consumer.credentials.tokenSecret
-    };
-    var message = {
-      action: form.action,
-      method: form.method,
-      parameters: []
-    };
-   /* for (var e = 0; e < form.elements.length; ++e) {
-        var input = form.elements[e];
-        if (input.name != null && input.name != "" && input.value != null
-            && (!(input.type == "checkbox" || input.type == "radio") || input.checked))
-        {
-            message.parameters.push([input.name, input.value]);
-        }
-    }
-    */
-    OAuth.setTimestampAndNonce(message);
-    OAuth.SignatureMethod.sign(message, accessor);
-    data = OAuth.getParameterMap(message.parameters);
-    /*for (var p in parameterMap) {
-        if (p.substring(0, 6) == "oauth_"
-         && form[p] != null && form[p].name != null && form[p].name != "")
-        {
-            form[p].value = parameterMap[p];
-        }
-    }*/
-    return "" + allplayers.consumer.credentials.serviceProvider.requestTokenURL;
+function request_token(site_url, consumerKey, consumerSecret) {
+  var url = site_url + allplayers.consumer.credentials.serviceProvider.requestTokenURL;
+  allplayers.consumer.credentials.consumerKey = consumerKey;
+  allplayers.consumer.credentials.consumerSecret = consumerSecret;
+  var accessor = {
+    consumerSecret: allplayers.consumer.credentials.consumerSecret,
+    tokenSecret   : allplayers.consumer.credentials.tokenSecret
+  };
+  var message = {
+    action: url,
+    method: "get",
+    parameters: [
+      ["oauth_consumer_key", allplayers.consumer.credentials.consumerKey],
+      ["oauth_signature_method","HMAC-SHA1"],
+      ["oauth_timestamp",""],
+      ["oauth_nonce",""],
+      ["oauth_signature",""]
+    ]
+  };
+  var query = {
+    url: url,
+    data: {}
+  };
+  OAuth.setTimestampAndNonce(message);
+  OAuth.SignatureMethod.sign(message, accessor);
+  query.data = OAuth.getParameterMap(message.parameters);
+  return query;
+};
+
+allplayers.consumer.access_token =
+function access_token(site_url, tokenKey, tokenSecret) {
+  var url = site_url + allplayers.consumer.credentials.serviceProvider.accessTokenURL;
+  allplayers.consumer.credentials.tokenKey = tokenKey;
+  allplayers.consumer.credentials.tokenSecret = tokenSecret;
+  var accessor = {
+    consumerSecret: allplayers.consumer.credentials.consumerSecret,
+    tokenSecret   : allplayers.consumer.credentials.tokenSecret
+  };
+  var message = {
+    action: url,
+    method: "get",
+    parameters: [
+      ["oauth_consumer_key", allplayers.consumer.credentials.consumerKey],
+      ["oauth_token", allplayers.consumer.credentials.tokenKey],
+      ["oauth_signature_method","HMAC-SHA1"],
+      ["oauth_timestamp",""],
+      ["oauth_nonce",""],
+      ["oauth_signature",""]
+    ]
+  };
+  var query = {
+    url: url,
+    data: {}
+  };
+  OAuth.setTimestampAndNonce(message);
+  OAuth.SignatureMethod.sign(message, accessor);
+  query.data = OAuth.getParameterMap(message.parameters);
+  return query;
+};
+
+allplayers.consumer.authorized_call =
+function authorized_call(site_url, tokenKey, tokenSecret) {
+  var url = site_url;
+  allplayers.consumer.credentials.tokenKey = tokenKey;
+  allplayers.consumer.credentials.tokenSecret = tokenSecret;
+  var accessor = {
+    consumerSecret: allplayers.consumer.credentials.consumerSecret,
+    tokenSecret   : allplayers.consumer.credentials.tokenSecret
+  };
+  var message = {
+    action: url,
+    method: "get",
+    parameters: [
+      ["oauth_consumer_key", allplayers.consumer.credentials.consumerKey],
+      ["oauth_token", allplayers.consumer.credentials.tokenKey],
+      ["oauth_signature_method","HMAC-SHA1"],
+      ["oauth_timestamp",""],
+      ["oauth_nonce",""],
+      ["oauth_signature",""]
+    ]
+  };
+  var query = {
+    url: url,
+    data: {}
+  };
+  OAuth.setTimestampAndNonce(message);
+  OAuth.SignatureMethod.sign(message, accessor);
+  query.data = OAuth.getParameterMap(message.parameters);
+  return query;
 };
